4c17a697fe915bbabf5f7c7e7f36a51dcff158bf
blur <thetobiterra@gmail.com>
2025-11-28 16:37:30 +0100

    schema

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
+import sampleData from "@/db/sample-data";
+import Image from "next/image";
+
 export default function Home() {
+  const products = sampleData.products;
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        Hello World
+    <div>
+      <main>
+        {products.map((product) => (
+          <div key={product.slug}>
+            <Image
+              src={product?.images?.[0]}
+              alt={product.name}
+              width={100}
+              height={100}
+            />
+            <div>
+              <p>{product.name}</p>
+              <p>{product.brand}</p>
+              <p>{product.price}</p>
+            </div>
+          </div>
+        ))}
       </main>
     </div>
   );
---
+++ app/refund-and-return-policy/page.tsx
@@ -0,0 +1,3 @@
+export default function RefundAndReturnPolicyPage() {
+  return <div></div>;
+}
