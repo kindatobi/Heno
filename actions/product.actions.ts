92ee8a940c74dd83ad1bd5c29619dc5cfed40564
blur <thetobiterra@gmail.com>
2025-12-14 16:52:39 +0100

    checkout product update function

--- actions/product.actions.ts
+++ actions/product.actions.ts
@@ -6,6 +6,8 @@ import { calcPrice } from "@/lib/utils";
 import { insertCartSchema } from "@/lib/validators";
 import { Cart, Product } from "@/types";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import Stripe from "stripe";
 
 export async function getProductBySlug(productSlug: string) {
   const data = (await prisma.product.findFirst({
@@ -56,7 +58,7 @@ export async function checkoutProduct(data: Cart | null) {
 
   const recalculatedCart = {
     items: vCart.items,
-    ...calcPrice(vCart.items), // Use the same calcPrice function
+    ...calcPrice(vCart.items),
   };
 
   if (hasAdjustments)
@@ -69,5 +71,40 @@ export async function checkoutProduct(data: Cart | null) {
   const sessionCartId = (await cookies()).get("sessionCartId")?.value;
   await redis.set(`cart-${sessionCartId}`, vCart);
 
-  // From here we create the stripe session
+  if (!process.env.STRIPE_SECRET_KEY) {
+    throw new Error("Missing Stripe secret key");
+  }
+
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2025-11-17.clover",
+  });
+
+  const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] =
+    vCart.items.map((item) => ({
+      price_data: {
+        currency: "usd",
+        unit_amount: item.price * 100,
+        product_data: {
+          name: item.name,
+          images: [item.image],
+        },
+      },
+      quantity: item.qty,
+    }));
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items,
+    mode: "payment",
+    success_url: `${
+      process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
+    }/success?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${
+      process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
+    }`,
+  });
+
+  if (session.url) {
+    redirect(session.url);
+  }
 }
