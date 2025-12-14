dff459d8ab7c812619a29a2a730872e60375ff93
blur <thetobiterra@gmail.com>
2025-12-14 20:26:03 +0100

    admin routes and proxy setup for dynamic price based in location

--- proxy.ts
+++ proxy.ts
@@ -1,14 +1,27 @@
-import { NextResponse } from "next/server";
-
-import type { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export function proxy(request: NextRequest) {
   const response = NextResponse.next();
 
   if (!request.cookies.get("sessionCartId")) {
-    const sessionCartId = crypto.randomUUID();
-    response.cookies.set("sessionCartId", sessionCartId);
+    response.cookies.set("sessionCartId", crypto.randomUUID());
   }
+
+  const existingCurrency = request.cookies.get("currency")?.value;
+  if (existingCurrency) {
+    return response;
+  }
+
+  const manualCurrency = request.cookies.get("manual_currency")?.value;
+  if (manualCurrency) {
+    response.cookies.set("currency", manualCurrency);
+    return response;
+  }
+
+  const country = request.headers.get("x-vercel-ip-country");
+  const currency = country === "NG" ? "NGN" : "USD";
+
+  response.cookies.set("currency", currency);
 
   return response;
 }
