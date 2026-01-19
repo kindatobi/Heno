c24a9fa38108376e53e15e1a9ba66e3fc77555b0
blur <thetobiterra@gmail.com>
2026-01-19 01:10:39 +0100

    added theme colour

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,11 @@ import ConditionalFooter from "@/components/conditional-footer";
 
 import Header from "@/components/header";
 import { getAllCategories, getProductsCount } from "@/lib/dal";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  themeColor: "#F5F6F4",
+};
 
 export default async function RootLayout({
   children,
